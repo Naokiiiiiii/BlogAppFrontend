dccf946630b0f0b9531e09a5e49c718a99fb7280
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-29 14:43:01 +0900

    feat: ログインまわり作成

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,52 +1,34 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect } from 'react'
 
 export const Home: FC = () => {
-
-  // const googleClientID = process.env.GOOGLE_CLIANT_ID
-  // console.log(googleClientID)
-
-  const responseGoogle = async () => {
-    const formResponse = await fetch("http://localhost:8080/login", {
-      method: "POST",
-    });
-    console.log(formResponse)
-  };
-
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:8080/login";
-  };
-
-  const handleLoginCallback = async () => {
-    const response = await fetch("http://localhost:3000/auth/google/callback?code=YOUR_AUTH_CODE");
-    const data = await response.json();
-    console.log(data)
-  };
+    window.location.href = 'http://localhost:8080/login'
+  }
 
   useEffect(() => {
     // URLから認証コードを取得
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
+    const urlParams = new URLSearchParams(window.location.search)
+    const code = urlParams.get('code')
 
     if (code) {
       // 認証コードをサーバーに送信
-      fetch('http://localhost:8080/callback', {
+      fetch('http://localhost:8080/token', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ code: code }),
       })
-        .then(response => response.json())
-        .then(data => {
-          const token = data;
-          console.log(data)
-          // 取得したトークンを使ってアプリの状態を更新したり、ユーザーをログイン状態にしたりします
+        .then((response) => response.json())
+        .then((data) => {
+          const token = data
+          console.log(token)
         })
-        .catch(error => {
-          // エラーハンドリング
-        });
+        .catch((error) => {
+          console.log('error', error)
+        })
     }
-  }, []); // 最初の1回だけ実行
+  }, [])
 
   return (
     <div className="App">
@@ -55,5 +37,5 @@ export const Home: FC = () => {
         <button onClick={handleGoogleLogin}>Sign in with Google</button>
       </header>
     </div>
-  );
+  )
 }
