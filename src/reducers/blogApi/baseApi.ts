affbd5600d876ff2b7b91d4a7c212d0eee01d76e
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-11-16 16:16:22 +0900

    feat: baseAPI作成

--- src/reducers/blogApi/baseApi.ts
+++ src/reducers/blogApi/baseApi.ts
@@ -1,78 +1,62 @@
-import { regenerateIdToken } from '@reducers/auth';
-import { selectAccessToken } from '@reducers/auth/selectors';
-import {
-  BaseQueryFn,
-  createApi,
-  FetchArgs,
-  fetchBaseQuery,
-  FetchBaseQueryError
-} from '@reduxjs/toolkit/query/react';
-import { RootState } from '@store/index';
-import { baseUrl } from '@utils/const';
-import httpStatus from 'http-status';
+import { regenerateIdToken } from '@reducers/auth'
+import { selectAccessToken } from '@reducers/auth/selectors'
+import { BaseQueryFn, createApi, FetchArgs, fetchBaseQuery, FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
+import { RootState } from '@store/index'
+import { baseUrl } from '@utils/const'
+import httpStatus from 'http-status'
 
-type Camelcase<S extends string> = S extends `${infer X}_${infer Y}${infer Z}`
-  ? `${Lowercase<X>}${Uppercase<Y>}${Camelcase<Z>}`
-  : Lowercase<S>;
+type Camelcase<S extends string> = S extends `${infer X}_${infer Y}${infer Z}` ? `${Lowercase<X>}${Uppercase<Y>}${Camelcase<Z>}` : Lowercase<S>
 
 // MEMO: 配列に対して適応できないので注意が必要
 export type KeysToCamelcase<T> = {
-  [K in keyof T as Camelcase<string & K>]: T[K] extends Record<string, unknown>
-    ? KeysToCamelcase<T[K]>
-    : T[K];
-};
+  [K in keyof T as Camelcase<string & K>]: T[K] extends Record<string, unknown> ? KeysToCamelcase<T[K]> : T[K]
+}
 
 //指定されたkeyの型を配列に変換する
 export type Arrayable<T, K extends keyof T> = {
-  [key in keyof T]: key extends K ? NonNullable<T[key]>[] : T[key];
-};
+  [key in keyof T]: key extends K ? NonNullable<T[key]>[] : T[key]
+}
 
 const baseQuery = fetchBaseQuery({
   baseUrl,
   prepareHeaders: (headers, { getState }) => {
-    const accessToken = selectAccessToken(getState() as RootState);
+    const accessToken = selectAccessToken(getState() as RootState)
     if (accessToken) {
-      headers.set('authorization', `Bearer ${accessToken}`);
+      headers.set('authorization', `Bearer ${accessToken}`)
     }
-    return headers;
+    return headers
   },
   paramsSerializer: (params) => {
     return Object.keys(params)
       .filter((k) => {
-        if (typeof params[k] === 'number' && params[k] >= 0) return true;
-        return params[k];
+        if (typeof params[k] === 'number' && params[k] >= 0) return true
+        return params[k]
       })
       .flatMap((k) => {
         if (params[k] instanceof Array) {
-          return params[k].map(
-            (v: string) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`
-          );
+          return params[k].map((v: string) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
         }
-        return `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`;
+        return `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`
       })
-      .join('&');
+      .join('&')
   },
 })
 
-const baseQueryWithReauth: BaseQueryFn<
-  string | FetchArgs,
-  unknown,
-  FetchBaseQueryError
-> = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions);
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions)
   if (result.error && result.error.status === httpStatus.UNAUTHORIZED) {
-    const res = await api.dispatch(regenerateIdToken());
+    const res = await api.dispatch(regenerateIdToken())
     if (res.meta.requestStatus === 'fulfilled') {
       // retry after refresh succeeded
-      result = await baseQuery(args, api, extraOptions);
+      result = await baseQuery(args, api, extraOptions)
     }
   }
-  return result;
-  };
+  return result
+}
 
 export const baseApi = createApi({
   reducerPath: 'shelfAppsApi',
   baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
   tagTypes: [],
-});
+})
