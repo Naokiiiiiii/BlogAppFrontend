38e5ed147c7918a0bdf56ca943833904b12b53a9
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-11-16 16:17:01 +0900

    feat: userApi作成

--- src/reducers/auth/index.ts
+++ src/reducers/auth/index.ts
@@ -1,92 +1,82 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { baseUrl } from "@utils/const";
-import { Token, TokenHandler } from "@utils/tokenHandler";
-import { User } from "types/user";
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { baseUrl } from '@utils/const'
+import { Token, TokenHandler } from '@utils/tokenHandler'
+import { User } from 'types/user'
 
 type AuthState = {
-  initialized: boolean;
-  tokens?: Token;
-  user?: User;
-};
+  initialized: boolean
+  tokens?: Token
+  user?: User
+}
 
-const tokenHandler = new TokenHandler();
+const tokenHandler = new TokenHandler()
 const initialState: AuthState = {
   initialized: false,
-};
+}
 
 const fetchNewIdToken = async (refreshToken: string) => {
-  const response = await fetch(`${baseUrl}regenerateToken`, {
+  const response = await fetch(`${baseUrl}/regenerateToken`, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
     },
     body: JSON.stringify({ refresh_token: refreshToken }),
-  });
+  })
   if (!response.ok) {
-    throw new Error('refresh session failed');
+    throw new Error('refresh session failed')
   }
-  return (await response.json()) as { id_token: string; access_token: string };
-};
+  return (await response.json()) as { id_token: string; access_token: string }
+}
 
-export const initialize = createAsyncThunk<{ token: Token } | undefined>(
-  'authn/initialize',
-  async () => {
-    const token = tokenHandler.getToken();
-    if (!token) {
-      return undefined;
-    }
-    // 初期ロード時に常に新しいトークンを取得する
-    try {
-      const { id_token, access_token } = await fetchNewIdToken(
-        token.refreshToken
-      );
-      return {
-        token: { ...token, idToken: id_token, accessToken: access_token },
-      };
-    } catch (err) {
-      console.error(err);
-    }
+export const initialize = createAsyncThunk<{ token: Token } | undefined>('authn/initialize', async () => {
+  const token = tokenHandler.getToken()
+  if (!token) {
+    return undefined
   }
-);
+  // 初期ロード時に常に新しいトークンを取得する
+  try {
+    const { id_token, access_token } = await fetchNewIdToken(token.refreshToken)
+    return {
+      token: { ...token, idToken: id_token, accessToken: access_token },
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
 
-export const regenerateIdToken = createAsyncThunk(
-  'auth/regenerateIdToken',
-  async () => {
-    const token = tokenHandler.getToken();
-    if (!token) {
-      throw new Error('no token');
-    }
-    const { id_token, access_token } = await fetchNewIdToken(
-      token.refreshToken
-    );
-    return { ...token, idToken: id_token, accessToken: access_token };
+export const regenerateIdToken = createAsyncThunk('auth/regenerateIdToken', async () => {
+  const token = tokenHandler.getToken()
+  if (!token) {
+    throw new Error('no token')
   }
-);
+  const { id_token, access_token } = await fetchNewIdToken(token.refreshToken)
+  return { ...token, idToken: id_token, accessToken: access_token }
+})
 
 const authSlice = createSlice({
   name: 'authn',
   initialState,
   reducers: {
     setUser: (state, action: PayloadAction<User>) => {
-      state.user = action.payload;
+      state.user = action.payload
     },
   },
   extraReducers: (builder) => {
     builder.addCase(initialize.fulfilled, (state, action) => {
-      state.initialized = true;
-      state.tokens = action.payload?.token;
-    });
+      state.initialized = true
+      state.tokens = action.payload?.token
+    })
     builder.addCase(initialize.rejected, (state) => {
-      state.initialized = true;
-    });
+      state.initialized = true
+    })
     builder.addCase(regenerateIdToken.fulfilled, (state, action) => {
-      state.tokens = action.payload;
-    });
+      state.tokens = action.payload
+    })
     builder.addCase(regenerateIdToken.rejected, (state) => {
       // note: intentionally sign out
-      state.tokens = undefined;
-      tokenHandler.clearToken();
-    });
+      state.tokens = undefined
+      tokenHandler.clearToken()
+    })
     //
     // builder.addMatcher(
     //   publicUsersApi.endpoints.signIn.matchFulfilled,
@@ -148,7 +138,7 @@ const authSlice = createSlice({
     //   tokenHandler.clearToken();
     // });
   },
-});
+})
 
-export const { setUser } = authSlice.actions;
-export const AuthReducer = authSlice.reducer;
+export const { setUser } = authSlice.actions
+export const AuthReducer = authSlice.reducer
