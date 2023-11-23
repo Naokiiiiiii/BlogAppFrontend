1e803a124229905d61ed1a10073161889b4bad03
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-11-23 22:25:08 +0900

    fix: ログイン前での画面処理追加

---
+++ src/components/organisms/secured/secured.tsx
@@ -0,0 +1,24 @@
+import { setUser } from '@reducers/auth'
+import { selectIsAuthenticated } from '@reducers/auth/selectors'
+import { usersApi } from '@reducers/blogApi/injections/userApi'
+import { useEffect } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { RootState, useAppDispatch, useAppSelector } from 'store'
+
+export const Secured = () => {
+  const dispatch = useAppDispatch()
+  const { user } = useAppSelector((state: RootState) => state.Auth)
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
+  useEffect(() => {
+    if (!user) {
+      dispatch(usersApi.endpoints.getUser.initiate({}))
+        .unwrap()
+        .then((data) => dispatch(setUser(data)))
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  })
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />
+}
