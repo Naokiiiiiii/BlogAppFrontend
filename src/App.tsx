3a087534740000459c1ab39f43abb8f132044902
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-01 19:55:41 +0900

    fix: providerの位置変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,20 @@
 import { Secured } from '@components/organisms/secured'
 import { Login } from '@components/pages/login'
 import React from 'react'
-import { Provider } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import { Article } from './components/pages/articles'
 import { Home } from './components/pages/home'
-import { store } from './store'
 
 const App = () => {
   return (
     <div>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Secured />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/articles" element={<Article />} />
-          </Route>
-        </Routes>
-      </Provider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Secured />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/articles" element={<Article />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
