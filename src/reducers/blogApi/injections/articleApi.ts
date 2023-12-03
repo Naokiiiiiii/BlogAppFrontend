87f7a26f960a8eae0da486934a1f8d7cc9dd9341
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-03 14:34:40 +0900

    feat: get article detail api作成

--- src/reducers/blogApi/injections/articleApi.ts
+++ src/reducers/blogApi/injections/articleApi.ts
@@ -3,6 +3,8 @@ import { baseApi } from '../baseApi'
 
 type GetArticleListParams = paths['/article/list']['get']['parameters']['query']
 type GetArticleListResponse = paths['/article/list']['get']['responses']['200']['content']['application/json']
+type GetArticleDetailParams = paths['/article/{article_id}']['get']['parameters']['path']
+type GetArticleDetailResponse = paths['/article/{article_id}']['get']['responses']['200']['content']['application/json']
 
 export const articlesApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -10,10 +12,16 @@ export const articlesApi = baseApi.injectEndpoints({
       query: (params) => ({
         url: 'article/list',
         method: 'GET',
-        params: { page: params?.page ?? 10 },
+        params: { page: params?.page },
+      }),
+    }),
+    getArticleDetail: builder.query<GetArticleDetailResponse, GetArticleDetailParams>({
+      query: (params) => ({
+        url: `article/${params.article_id}`,
+        method: 'GET',
       }),
     }),
   }),
 })
 
-export const { useGetArticlesQuery } = articlesApi
+export const { useGetArticlesQuery, useGetArticleDetailQuery } = articlesApi
