204a491ce8d70e905d791cf8861e0c507c16154f
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-04 16:20:56 +0900

    fix: home項目追加

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,6 +1,12 @@
+import { Box } from '@mui/material'
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Home: FC = () => {
-  return <Link to="/articles">記事一覧</Link>
+  return (
+    <Box component="div">
+      <Link to="/articles">記事一覧</Link>
+      <Link to="/article/create">記事新規作成</Link>
+    </Box>
+  )
 }
