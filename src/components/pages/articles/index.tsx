85ffe93460cbc5d6dd8bb8fac1454e829c629953
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-04 23:18:56 +0900

    feat: deleteArticle組み込み

--- src/components/pages/articles/index.tsx
+++ src/components/pages/articles/index.tsx
@@ -1,13 +1,17 @@
 import { Box, Button, Typography } from '@mui/material'
-import { useGetArticlesQuery } from '@reducers/blogApi/injections/articleApi'
+import { useDeleteArticleMutation, useGetArticlesQuery } from '@reducers/blogApi/injections/articleApi'
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export const Articles: FC = () => {
   const { data: articles, isLoading } = useGetArticlesQuery({})
+  const [deleteArticle] = useDeleteArticleMutation()
   const navigate = useNavigate()
   const handleClickRow = (id: number) => {
     navigate(`${id}`)
+  }
+  const handleClickDelete = async (id: number) => {
+    await deleteArticle({ article_id: id })
   }
   return (
     <Box>
@@ -16,6 +20,7 @@ export const Articles: FC = () => {
           <Typography>{article.title}</Typography>
           <Typography>{article.contents}</Typography>
           <Button onClick={() => handleClickRow(article.id)}>詳細</Button>
+          <Button onClick={() => handleClickDelete(article.id)}>削除</Button>
         </Box>
       ))}
     </Box>
