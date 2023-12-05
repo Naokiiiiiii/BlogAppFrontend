67200139220e582e47dcfa78fdcbffbbeafda8a3
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-05 16:47:02 +0900

    fix: コメント追加

--- src/components/pages/articleDetail/index.tsx
+++ src/components/pages/articleDetail/index.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { CircularProgress, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import { useGetArticleDetailQuery } from '@reducers/blogApi/injections/articleApi'
 import { FC } from 'react'
@@ -10,25 +10,31 @@ export const ArticleDetail: FC = () => {
 
   return (
     <Box>
-      <Typography>article: {article?.id}</Typography>
-      <Typography>title: {article?.title}</Typography>
-      <Typography>contents: {article?.contents}</Typography>
-      <Typography>created_at: {article?.created_at}</Typography>
-      <Typography>updated_at: {article?.updated_at}</Typography>
-      <Box>
-        <Typography>コメント一覧</Typography>
-        {article?.comments ? (
-          article?.comments.map((comment) => (
-            <Box key={comment.comment_id}>
-              <Typography>
-                {comment.message} {comment.user_name}
-              </Typography>
-            </Box>
-          ))
-        ) : (
-          <Typography>コメントなし</Typography>
-        )}
-      </Box>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <Box>
+          <Typography>article: {article?.id}</Typography>
+          <Typography>title: {article?.title}</Typography>
+          <Typography>contents: {article?.contents}</Typography>
+          <Typography>created_at: {article?.created_at}</Typography>
+          <Typography>updated_at: {article?.updated_at}</Typography>
+          <Box>
+            <Typography>コメント一覧</Typography>
+            {article?.comments && article.comments.length > 0 ? (
+              article.comments.map((comment) => (
+                <Box key={comment.comment_id}>
+                  <Typography>
+                    {comment.message} {comment.user_name}
+                  </Typography>
+                </Box>
+              ))
+            ) : (
+              <Typography>コメントなし</Typography>
+            )}
+          </Box>
+        </Box>
+      )}
     </Box>
   )
 }
