a82383080cb87b38b12a6c2a7e6fb1014722ecba
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-08 09:56:57 +0900

    feat: updateusername api作成

--- src/reducers/blogApi/injections/userApi.ts
+++ src/reducers/blogApi/injections/userApi.ts
@@ -3,6 +3,9 @@ import { baseApi } from '../baseApi'
 
 type GetUserInfoParams = {}
 type GetUserInfoResponse = paths['/user']['get']['responses']['200']['content']['application/json']
+type UpdateUserNameParams = paths['/user/{user_id}']['put']['parameters']['path'] &
+  paths['/user/{user_id}']['put']['requestBody']['content']['application/json']
+type UpdateUserNameResponse = paths['/user/{user_id}']['put']['responses']['200']['content']
 
 export const usersApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -11,5 +14,16 @@ export const usersApi = baseApi.injectEndpoints({
         url: 'user',
       }),
     }),
+    updateUserName: builder.mutation<UpdateUserNameResponse, UpdateUserNameParams>({
+      query: (params) => ({
+        url: `user/${params.user_id}`,
+        method: 'PUT',
+        body: {
+          user_name: params.user_name,
+        },
+      }),
+    }),
   }),
 })
+
+export const { useUpdateUserNameMutation } = usersApi
